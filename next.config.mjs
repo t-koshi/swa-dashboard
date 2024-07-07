7bbd3e4c89fd1d5fdf05dfa7c37ca9e7a3a3e8cb
t-koshi <5916757+t-koshi@users.noreply.github.com>
2024-07-08 01:34:28 +0900

    main

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+/* 公開時のサブディレクトリ */
+const SUB_DIRECTORY = "/dashboard";
+
+/* 本番環境と開発環境の分岐用のフラグ */
+const isProd = process.env.NODE_ENV == "production"
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  assetPrefix: isProd ? SUB_DIRECTORY : "",
+};
 
 export default nextConfig;
